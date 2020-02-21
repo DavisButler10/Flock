ea0d6ae7200eba0d3ed370f9e6002149d66cc1f3
DavisButler10 <45404502+DavisButler10@users.noreply.github.com>
2020-02-21 12:09:53 -0700

    Add files via upload

---
+++ Seek.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : SteeringBehavior
+{
+    public Kinematic character;
+    public GameObject target;
+
+    public float maxAcceleration = 1f;
+    public float maxSpeed = 1f;
+
+    public bool flee = false;
+
+    protected virtual Vector3 getTargetPosition()
+    {
+        return target.transform.position;
+    }
+
+    public override SteeringOutput getSteering()
+    {
+        SteeringOutput result = new SteeringOutput();
+
+
+        result.linear = getTargetPosition() - character.transform.position;
+        //Debug.Log(result.linear);
+        if (result.linear.magnitude >= maxSpeed)
+        {
+            result.linear.Normalize();
+            result.linear *= maxAcceleration;
+        }
+        else {
+            result.linear.Normalize();
+        }
+        
+
+        result.angular = 0;
+        return result;
+    }
+}
